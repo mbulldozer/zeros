51f3dc0c80b1682868374be7a16b0ea7a704a446
mbulldozer <mbulldozer@mail.ru>
2018-09-27 23:15:40 +0300

    successfull

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let zerosCount= 0;
+  while(number > 0){
+    number = Math.floor(number / 5);
+    zerosCount += number;
+  }
+  return zerosCount;
 }
